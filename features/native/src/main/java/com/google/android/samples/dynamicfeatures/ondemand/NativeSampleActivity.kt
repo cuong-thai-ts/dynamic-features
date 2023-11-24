130c03bc6deef465bb0322458c04f9f2cd5e1e72
Cuong Thai <cuong.thai@trustingsocial.com>
2023-11-24 12:53:11 +0700

    Successfully applied soloader from facebook

--- features/native/src/main/java/com/google/android/samples/dynamicfeatures/ondemand/NativeSampleActivity.kt
+++ features/native/src/main/java/com/google/android/samples/dynamicfeatures/ondemand/NativeSampleActivity.kt
@@ -16,36 +16,63 @@
 
 package com.google.android.samples.dynamicfeatures.ondemand
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import com.google.android.play.core.splitinstall.SplitInstallHelper
 import com.google.android.samples.dynamicfeatures.BaseSplitActivity
 import com.google.android.samples.dynamicfeatures.ondemand.ccode.R
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 /** A simple activity displaying some text coming through via JNI. */
 class NativeSampleActivity : BaseSplitActivity() {
 
+    @SuppressLint("NewApi")
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase)
         val libName = "trustvision-lib"
         val libc = "c++_shared"
+        val libcPath =
+            "/data/data/com.google.android.samples.dynamicfeatures.ondemand/files/splitcompat/1/native-libraries/native.config.arm64_v8a/libc++_shared.so"
+//        val libcPath = "/data/user/0/com.google.android.samples.dynamicfeatures.ondemand/files/splitcompat/1/native-libraries/native.config.arm64_v8a/libc++_shared.so"
 //        val libc = "hello-jni"
+        var libFile: File? = null
         try {
-            Log.i("TdcTest", "Loading...$libName along with c++_shared")
+            Log.i("TdcTest", "Loading...$libName along with ${File(libcPath).exists()}: $libcPath ")
+//            println("TdcTest ~ source dirs: ${applicationInfo.splitSourceDirs.joinToString(", ")}")
+            println("TdcTest ~ files dir: $filesDir")
 //            System.loadLibrary(libc)
-            System.load("/data/data/com.google.android.samples.dynamicfeatures.ondemand/files/splitcompat/1/native-libraries/native.config.arm64_v8a/libc++_shared.so")
-            Log.i("TdcTest", "Loaded...$libc")
-//            System.loadLibrary(libName)
-            Log.i("TdcTest", "Loaded...$libName")
+            libFile = filesDir.find(libName)
+            println("TdcTest ~ libPath: $libFile")
+            System.load(libFile?.path ?: libcPath)
+            Log.i("TdcTest", "Loaded...")
         } catch (e: Throwable) {
             Log.e("TdcTest", "traditional library load failed...split loading...")
             e.printStackTrace()
-            SplitInstallHelper.loadLibrary(this, libc)
+//            initSo(libcPath)
+            initSo(libFile?.path ?: "/data/data/com.google.android.samples.dynamicfeatures.ondemand/files/splitcompat/1/native-libraries/native.config.arm64_v8a/libtrustvision-lib.so")
+//            SplitInstallHelper.loadLibrary(this, libc)
 //            libraryFallback(libc, this)
 //            libraryFallback(libName, this)
         }
+    }
+
+    private fun File.find(name: String): File? {
+        if (this.name.contains(name, true)) return this
+        if (!isDirectory) return null
+        listFiles()?.forEach { return it.find(name) ?: return@forEach }
+        return null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,6 +82,88 @@ class NativeSampleActivity : BaseSplitActivity() {
         setContentView(R.layout.activity_hello_jni)
         findViewById<TextView>(R.id.hello_textview).text = stringFromJNI()
     }
+
+    @Synchronized
+    private fun initSo(path: String) {
+        val runtime = Runtime.getRuntime()
+        val nativeLoadRuntimeMethod: Method = getNativeLoadRuntimeMethod() ?: return
+        var error: String? = null
+        try {
+            synchronized(runtime) {
+                error = nativeLoadRuntimeMethod.invoke(
+                    runtime, path, javaClass.classLoader, path
+                ) as String
+                if (error != null) {
+                    throw UnsatisfiedLinkError(error)
+                }
+            }
+        } catch (e: IllegalAccessException) {
+            error = "Error: Cannot load $path"
+            throw RuntimeException(error, e)
+        } catch (e: IllegalArgumentException) {
+            error = "Error: Cannot load $path"
+            throw RuntimeException(error, e)
+        } catch (e: InvocationTargetException) {
+            error = "Error: Cannot load $path"
+            throw RuntimeException(error, e)
+        } finally {
+            if (error != null) {
+                Log.e(
+                    "TdcTest",
+                    "Error when loading lib: "
+                            + error
+                            + " lib hash: "
+                            + getLibHash(path)
+                            + " search path is "
+                            + path
+                )
+            }
+        }
+    }
+
+    /** * Logs MD5 of lib that failed loading  */
+    private fun getLibHash(libPath: String): String {
+        var digestStr: String
+        try {
+            val libFile = File(libPath)
+            val digest = MessageDigest.getInstance("MD5")
+            FileInputStream(libFile).use { libInStream ->
+                val buffer = ByteArray(4096)
+                var bytesRead: Int
+                while (libInStream.read(buffer).also { bytesRead = it } > 0) {
+                    digest.update(buffer, 0, bytesRead)
+                }
+                digestStr =
+                    String.format("%32x", BigInteger(1, digest.digest()))
+            }
+        } catch (e: IOException) {
+            digestStr = e.toString()
+        } catch (e: SecurityException) {
+            digestStr = e.toString()
+        } catch (e: NoSuchAlgorithmException) {
+            digestStr = e.toString()
+        }
+        return digestStr
+    }
+
+    private fun getNativeLoadRuntimeMethod(): Method? {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Build.VERSION.SDK_INT > 27) {
+            null
+        } else try {
+            Runtime::class.java.getDeclaredMethod(
+                "nativeLoad", String::class.java, ClassLoader::class.java, String::class.java
+            ).also {
+                it.isAccessible = true
+            }
+        } catch (e: NoSuchMethodException) {
+            Log.w("TdcTest", "Cannot get nativeLoad method", e)
+            null
+        } catch (e: SecurityException) {
+            Log.w("TdcTest", "Cannot get nativeLoad method", e)
+            null
+        }
+    }
+
 
     /** Read a string from packaged native code. */
     external fun stringFromJNI(): String
